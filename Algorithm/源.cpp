50e2bc1362bbdecb9e09be3349c50758316e6250
Huan Liang <lianghuan18391713434@gmail.com>
2021-03-10 23:10:22 +0800

    添加项目文件。

---
+++ Algorithm/源.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+int main() {
+	using namespace std;
+
+	int sum = 0;
+	cout << "请输入一串数字和任意数目的空格；";
+	int i;
+	while (cin >> i)
+	{
+		sum += i;
+		while( cin.peek() == ' ' )
+		{
+			cin.get();
+		}
+		if( cin.peek() == '\n' )
+		{
+			break;
+
+		}
+	}
+
+
+	cout << "结果是" << sum << endl;
+	
+	return 0;
+}
